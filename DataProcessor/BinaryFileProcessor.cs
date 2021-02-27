cded4c09ca57487741506855b7ce1655930413c5
Jordan Humphrey <jphumphrey2@gmail.com>
2021-02-26 22:00:29 -0800

    Add project files.

---
+++ DataProcessor/BinaryFileProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Abstractions;
+
+namespace DataProcessor
+{
+    public class BinaryFileProcessor
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public string InputFilePath { get; }
+        public string OutputFilePath { get; }
+
+        public BinaryFileProcessor(string inputFilePath, string outputFilePath)
+            : this(inputFilePath, outputFilePath, new FileSystem()) { }
+
+        public BinaryFileProcessor(string inputFilePath, string outputFilePath,
+            IFileSystem fileSystem)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilePath = outputFilePath;
+            _fileSystem = fileSystem;
+        }
+
+        /// <summary>
+        /// Simply finds the largest byte in the file and appends it to the end of the file
+        /// </summary>
+        public void Process()
+        {
+            using (Stream inputFileStream = _fileSystem.File.Open(InputFilePath, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binaryStreamReader = new BinaryReader(inputFileStream))
+            using (Stream outputFileStream = _fileSystem.File.Create(OutputFilePath))
+            using (BinaryWriter binaryStreamWriter = new BinaryWriter(outputFileStream))
+            {
+                byte largest = 0;
+
+                // If we have more bytes to read, keep looping
+                while (binaryStreamReader.BaseStream.Position < binaryStreamReader.BaseStream.Length)
+                {
+                    byte currentByte = binaryStreamReader.ReadByte();
+
+                    binaryStreamWriter.Write(currentByte);
+
+                    if (currentByte > largest)
+                    {
+                        largest = currentByte;
+                    }
+                }
+
+                binaryStreamWriter.Write(largest);
+            }
+        }
+    }
+}
---
+++ DataProcessor/TextFileProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Abstractions;
+
+namespace DataProcessor
+{
+    public class TextFileProcessor
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public string InputFilePath { get; }
+        public string OutputFilePath { get; }
+
+        // production code uses this
+        public TextFileProcessor(string inputFilePath, string outputFilePath)
+            : this(inputFilePath, outputFilePath, new FileSystem()) { }
+
+        // testing code uses this
+        public TextFileProcessor(string inputFilePath, string outputFilePath, IFileSystem fileSystem)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilePath = outputFilePath;
+            _fileSystem = fileSystem;
+        }
+
+        /// <summary>
+        /// Simply finds the second line of a text file and converts it to uppercase
+        /// </summary>
+        public void Process()
+        {
+            using (var inputStreamReader = _fileSystem.File.OpenText(InputFilePath))
+            using (var outputStreamWriter = _fileSystem.File.CreateText(OutputFilePath))
+            {
+                var currentLineNumer = 1;
+                while (!inputStreamReader.EndOfStream)
+                {
+                    string line = inputStreamReader.ReadLine();
+
+                    if (currentLineNumer == 2)
+                    {
+                        Write(line.ToUpperInvariant());
+                    }
+                    else
+                    {
+                        Write(line);
+                    }
+
+                    currentLineNumer++;
+
+                    void Write(string content)
+                    {
+                        bool isLastLine = inputStreamReader.EndOfStream;
+
+                        if (isLastLine)
+                        {
+                            outputStreamWriter.Write(content);
+                        }
+                        else
+                        {
+                            outputStreamWriter.WriteLine(content);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
